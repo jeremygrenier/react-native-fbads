6d2e2f90251078204c2dbf460de95c4fa80578ca
Jeremy Grenier <jeremy@flashgap.com>
2018-05-18 11:29:08 +0200

    Fix interstitial ad launch in background thread on iOS

--- src/ios/CTKInterstitialAdManager.m
+++ src/ios/CTKInterstitialAdManager.m
@@ -24,50 +24,53 @@
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(
-  showAd:(NSString *)placementId
-  resolver:(RCTPromiseResolveBlock)resolve
-  rejecter:(RCTPromiseRejectBlock)reject
-) {
-  if (_resolve != nil && _reject != nil) {
-    reject(@"E_FAILED_TO_SHOW", @"Only one `showAd` can be called at once", nil);
-    return;
-  }
-  
-  _resolve = resolve;
-  _reject = reject;
-  
-  _interstitialAd = [[FBInterstitialAd alloc] initWithPlacementID:placementId];
-  _interstitialAd.delegate = self;
-  [_interstitialAd loadAd];
+                  showAd:(NSString *)placementId
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  ) {
+    if (_resolve != nil && _reject != nil) {
+        reject(@"E_FAILED_TO_SHOW", @"Only one `showAd` can be called at once", nil);
+        return;
+    }
+
+    _resolve = resolve;
+    _reject = reject;
+    
+    _interstitialAd = [[FBInterstitialAd alloc] initWithPlacementID:placementId];
+    _interstitialAd.delegate = self;
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [_interstitialAd loadAd];
+    });
 }
 
 #pragma mark - FBInterstitialAdDelegate
 
 - (void)interstitialAdDidLoad:(FBInterstitialAd *)interstitialAd {
-  [interstitialAd showAdFromRootViewController:RCTPresentedViewController()];
+    [interstitialAd showAdFromRootViewController:RCTPresentedViewController()];
 }
 
 - (void)interstitialAd:(FBInterstitialAd *)interstitialAd didFailWithError:(NSError *)error {
-  _reject(@"E_FAILED_TO_LOAD", [error localizedDescription], error);
-  
-  [self cleanUpPromise];
+    _reject(@"E_FAILED_TO_LOAD", [error localizedDescription], error);
+
+    [self cleanUpPromise];
 }
 
 - (void)interstitialAdDidClick:(FBInterstitialAd *)interstitialAd {
-  _didClick = true;
+    _didClick = true;
 }
 
 - (void)interstitialAdDidClose:(FBInterstitialAd *)interstitialAd {
-  _resolve(@(_didClick));
-  
-  [self cleanUpPromise];
+    _resolve(@(_didClick));
+
+    [self cleanUpPromise];
 }
 
 - (void)cleanUpPromise {
-  _reject = nil;
-  _resolve = nil;
-  _interstitialAd = nil;
-  _didClick = false;
+    _reject = nil;
+    _resolve = nil;
+    _interstitialAd = nil;
+    _didClick = false;
 }
 
 @end
