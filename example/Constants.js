3c704ced40c3c4b2e0d27b2fe500b0fd436beeff
Jeremy Grenier <jeremy@flashgap.com>
2018-05-18 11:26:30 +0200

    Use your own Ads IDs in example project

---
+++ example/Constants.js
@@ -0,0 +1,7 @@
+let ADS_ID = {
+    banner: "[Banner Ad ID]",
+    interstitial: "[Interstitial Ad ID]",
+    native: "[Native Ad ID]"
+};
+
+module.exports = { ADS_ID };
--- example/index.js
+++ example/index.js
@@ -4,71 +4,69 @@
  * @flow
  */
 
-import React from 'react';
-import { AppRegistry, StyleSheet, Text, TouchableOpacity, ScrollView } from 'react-native';
+import React from "react";
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, ScrollView } from "react-native";
 
-import FullAd from './components/FullAd';
-import { NativeAdsManager, InterstitialAdManager, BannerView, AdSettings } from '../';
+import FullAd from "./components/FullAd";
+import { NativeAdsManager, InterstitialAdManager, BannerView, AdSettings } from "../";
+import { ADS_ID } from "./Constants";
 
 AdSettings.addTestDevice(AdSettings.currentDeviceHash);
 
-const adsManager = new NativeAdsManager('1912255062335197_1912257885668248');
+const adsManager = new NativeAdsManager(ADS_ID.native);
 
 class MainApp extends React.Component {
-  showFullScreenAd = () => {
-    InterstitialAdManager.showAd('1912255062335197_1914986612062042')
-      .then(didClick => {
-        console.log(didClick);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+    showFullScreenAd = () => {
+        InterstitialAdManager.showAd(ADS_ID.interstitial)
+            .then(didClick => {
+                console.log(didClick);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
 
-  onBannerAdPress = () => console.log('Ad clicked!');
-  onBannerAdError = (event) => console.log('Ad error :(', event.nativeEvent);
+    onBannerAdPress = () => console.log("Ad clicked!");
+    onBannerAdError = event => console.log("Ad error :(", event.nativeEvent);
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.p}>
-          One of the coolest features about native ads
-          is that they nicely integrate with the general
-          app look & feel
-        </Text>
-        <FullAd adsManager={adsManager} />
-        <TouchableOpacity onPress={this.showFullScreenAd}>
-          <Text>Show interstitial ad</Text>
-        </TouchableOpacity>
-        <BannerView
-          type="large"
-          placementId="1912255062335197_1954647211429315"
-          onPress={this.onBannerAdPress}
-          onError={this.onBannerAdError}
-        />
-      </ScrollView>
-    );
-  }
+    render() {
+        return (
+            <ScrollView style={styles.container}>
+                <Text style={styles.welcome}>Welcome to React Native!</Text>
+                <Text style={styles.p}>
+                    One of the coolest features about native ads is that they nicely integrate with the general app look
+                    & feel
+                </Text>
+                <FullAd adsManager={adsManager} />
+                <TouchableOpacity onPress={this.showFullScreenAd}>
+                    <Text>Show interstitial ad</Text>
+                </TouchableOpacity>
+                <BannerView
+                    type="large"
+                    placementId={ADS_ID.banner}
+                    onPress={this.onBannerAdPress}
+                    onError={this.onBannerAdError}
+                />
+            </ScrollView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF'
-  },
-  p: {
-    marginBottom: 10,
-    marginHorizontal: 40,
-    textAlign: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#F5FCFF"
+    },
+    p: {
+        marginBottom: 10,
+        marginHorizontal: 40,
+        textAlign: "center"
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10
+    }
 });
 
-AppRegistry.registerComponent('MainApp', () => MainApp);
+AppRegistry.registerComponent("MainApp", () => MainApp);
